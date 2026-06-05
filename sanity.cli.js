b4977b8fb3f5037ac373d7883c48ca8e72dc4b50
Finabluma <oscar.francino@finabluma.com>
2026-06-05 19:28:26 +0200

    .

--- sanity.cli.js
+++ sanity.cli.js
@@ -1,16 +1,17 @@
 /* eslint-disable no-undef */
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-    dataset:process.env.SANITY_STUDIO_DATASET 
+    dataset: process.env.SANITY_STUDIO_DATASET,
   },
   deployment: {
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
+    appId: 'vc4685zpg9c2h5fqid6a7412',
     autoUpdates: true,
-  }
+  },
 })
